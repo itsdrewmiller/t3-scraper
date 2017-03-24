c6ac0b4f0e1ccd7d59d590d333e3a8e86800c2ce
shaisachs <ssachs@ngpvan.com>
2017-03-24 16:25:14 -0400

    Crawl every year
    Signed-off-by: shaisachs <ssachs@ngpvan.com>

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var fs = require('fs');
 var _ = require('lodash');
 var cheerio = require('cheerio');
 
-crawlYear(2000);
+var years = [2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006,2005,2004,2003,2002,2001,2000,1996,1995,1994,1993,1992,1991,1987,1986,1985,1984];
+_.forEach(years, crawlYear);
 
 function crawlYear(year) {
 	var url = 'https://triticeaetoolbox.org/barley/view_search_yr2.php?table=experiments&year=' + year;
